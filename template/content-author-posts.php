c43541644f7773045f70ff30145f2c7d1f9d9861
Ty <thopp@polity.tech>
2018-01-07 16:12:12 -0800

    change from live to staging site to avoid cache, pause before implement final mobile bio portion

--- template/content-author-posts.php
+++ template/content-author-posts.php
@@ -38,7 +38,7 @@ $curauth = $wp_query->get_queried_object();  // get current author
 					if($loop-> have_posts() ) :  while($loop->have_posts()) : $loop->the_post(); 
 			?>
 							
-			<?php  $category = get_the_category(); ?>		
+			<?php $category = get_the_category(); ?>		
 		 
 				<article  class="post-content col-md-4">
 						
@@ -49,11 +49,21 @@ $curauth = $wp_query->get_queried_object();  // get current author
 					</figure>
 						
 					<div class="blog-post">						
-						
-						<span class="blog-post-cat blog-post-meta <?php echo $category[0]->slug; ?>"><?php  get_category_link($category[0]->cat_ID); ?><?php the_category(', ');?></span><span class="read-time"><?php echo high_reading_time();?></span>		
-						<h2 class="post-meta-title"><a href="<?php echo get_permalink( $post->ID ); ?>"><?php the_title(); ?></a></h2>				
-						<span class="blog-post-date post-date small"><?php echo get_the_date(); ?></span>
-					
+						<span class="blog-post-cat blog-post-meta">
+							<?php  get_category_link($category[0]->cat_ID); ?>
+							<?php the_category(', ');?>							
+						</span>
+						<span class="read-time">
+							<?php echo high_reading_time();?>
+						</span>		
+						<h2 class="post-meta-title">
+							<a href="<?php echo get_permalink( $post->ID ); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h2>				
+						<span class="blog-post-date post-date small">
+							<?php echo get_the_date(); ?>
+						</span>
 					</div>		
 						   
 				</article>
