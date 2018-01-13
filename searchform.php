4e02f6d062f0ce3b2cf0213c17f8256cc8cd56c7
Ty <thopp@polity.tech>
2018-01-13 11:13:07 -0800

    enlarge search text box size, add autofocus and remove onblur/onfocus funcs

--- searchform.php
+++ searchform.php
@@ -5,14 +5,15 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<span class="label"></span>
-		<label> 
-	  	<input 
-	  		type="search" 
-	  		autofocus="true"
-	  		class="field s" 
-	  		name="s" 
-	  		value="<?php _e( '', 'frame-blog' ); ?>" 
-	  	/>
+		<label class="search-label"> 
+			Type your search and press enter...
 		</label>
+  	<input 
+  		type="search" 
+  		autofocus="true"
+  		class="field s" 
+  		name="s" 
+  		value="<?php _e( '', 'frame-blog' ); ?>" 
+  	/>
 	<input type="hidden" id="searchsubmit" /> 
 </form>
